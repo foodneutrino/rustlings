a0bb372ea798a1d9da58e80c31ce2d2df196a984
David laBarbera <davidlabarbera@Davids-MacBook-Pro.local>
2022-07-09 10:29:44 -0400

    up to generics 2

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -4,18 +4,22 @@
 // It won't compile right now! Why?
 // Execute `rustlings hint errors5` for hints!
 
-// I AM NOT DONE
-
 use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), ParseIntError> {
+fn main() -> Result<(), CreationError> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
+}
+
+impl From<ParseIntError> for CreationError {
+    fn from(_: ParseIntError) -> CreationError {
+        CreationError::Unknown
+    }
 }
 
 // Don't change anything below this line.
@@ -27,6 +31,7 @@ struct PositiveNonzeroInteger(u64);
 enum CreationError {
     Negative,
     Zero,
+    Unknown,
 }
 
 impl PositiveNonzeroInteger {
@@ -45,6 +50,7 @@ impl fmt::Display for CreationError {
         let description = match *self {
             CreationError::Negative => "number is negative",
             CreationError::Zero => "number is zero",
+            CreationError::Unknown => "value not a number"
         };
         f.write_str(description)
     }
--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,8 +8,6 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -19,8 +17,10 @@ enum ParsePosNonzeroError {
     ParseInt(ParseIntError)
 }
 
-impl ParsePosNonzeroError {
-    // TODO: add another error conversion function here.
+impl From<ParseIntError> for ParsePosNonzeroError {
+    fn from(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str)
@@ -28,9 +28,9 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse()?;
     PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+        .map_err(ParsePosNonzeroError::Creation)
 }
 
 // Don't change anything below this line.
